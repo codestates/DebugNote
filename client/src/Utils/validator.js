4f975deca46ddd0fab19399c6b8ebedf91565e74
JeongTaeYeong <bellamy828@gmail.com>
2022-04-07 20:03:11 +0900

    Client [ADD] mainpage auth

---
+++ client/src/Utils/validator.js
@@ -0,0 +1,30 @@
+module.exports = {
+  idValidator: id => {
+    const regExp = /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/;
+    return regExp.test(id);
+  },
+
+  pwValidator: pw => {
+    const regExp = /^(?=.[A-Za-z])(?=.\d)[A-Za-z\d]{8,20}$/;
+    return regExp.test(pw);
+  },
+
+  pwMatchValidator: (pw, pwconfirm) => {
+    return pw === pwconfirm;
+  },
+
+  nicknameValidator: nickname => {
+    const regExp = /^[a-zA-Zㄱ-ㅎㅏ-ㅣ가-힣]{3,20}$/;
+
+    return regExp.test(nickname);
+  },
+
+  nameValidator: name => {
+    return name !== '';
+  },
+
+  selectValidator: option => {
+    console.log(option === '직업을 선택하세요');
+    return option !== '직업을 선택하세요';
+  },
+};
