a063c3adf15e5222c3ec7f450fb5295c05798f9b
JeongTaeYeong <bellamy828@gmail.com>
2022-04-07 23:09:09 +0900

    Client [ADD] mainpage ErrorLog Component

---
+++ client/src/Components/ErrorLog.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function ErrorLog() {
+  return (
+    <article key="" className="errlog">
+      <div className="bookmark-count">북마크 수</div>
+      <div>
+        <div className="errlog-title-summary">게시글 제목 일부</div>
+        <div className="errlog-content-summary">게시글 본문 일부</div>
+      </div>
+    </article>
+  );
+}
