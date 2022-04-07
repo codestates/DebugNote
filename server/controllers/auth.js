a1061b23b4725fc6d32944f76d2d84d9ba0edff4
Jaehyeon <parkjh9370@naver.com>
2022-04-07 15:32:05 +0900

    server [ADD] comment code

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,68 +1,43 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const config = require('../config')
+const config = require('../config');
 const User = require('../models/user');
 
 module.exports = {
   // 회원가입
-  signup: async (req, res) =>{
-      const { email, password, nickname, name, job } = req.body;
+  signup: async (req, res) => {
+    const { email, password, nickname, name, job } = req.body;
 
-      if (!email) {
-        return res.status(401).json({ message: '이메일 필수 입력' });
-      }
-      if (!password) {
-        return res.status(401).json({ message: '비밀번호 필수 입력' });
-      }
-      if (!name) {
-        return res.status(401).json({ message: '이름 필수 입력' });
-      }
-      if (!nickname) {
-        return res.status(401).json({ message: '닉네임 필수 입력' });
-      }
-      if (!job) {
-        return res.status(401).json({ message: '직업 필수 입력' });
-      }
-      // validation (nickname, email, password ...) 중복 확인 필요
-      const user = await User.findOne({
-        where: {
-          email,
-        },
-      });
-
-      if (user) {
-        return res.status(409).json({ message: '중복된 유저 입니다.' });
-      }
-      const hashedPassword = await bcrypt
-        .hash(password, config.bcrypt.saltRounds)
-        .catch(err => console.log(err));
-
-      const newUser = await User.create({
+    // validation (nickname, email, password ...) 중복 확인 필요
+    const user = await User.findOne({
+      where: {
         email,
-        password: hashedPassword,
-        nickname,
-        name,
-        job,
-      });
-      return res
-        .status(201)
-        .json({ id: newUser.id, message: '유저가 생성 되었습니다.' });
-    },
+      },
+    });
+
+    if (user) {
+      return res.status(409).json({ message: '중복된 유저 입니다.' });
+    }
+    const hashedPassword = await bcrypt
+      .hash(password, config.bcrypt.saltRounds)
+      .catch(err => console.log(err));
+
+    const newUser = await User.create({
+      email,
+      password: hashedPassword,
+      nickname,
+      name,
+      job,
+    });
+    return res
+      .status(201)
+      .json({ id: newUser.id, message: '유저가 생성 되었습니다.' });
+  },
   // 회원탈퇴
-  signout: async (req, res) =>{
-    
-  },  
+  signout: async (req, res) => {},
   // 로그인
   login: async (req, res) => {
     const { email, password } = req.body;
-
-    if (!email) {
-      return res.status(401).json({ message: '이메일 필수 입력' });
-    }
-
-    if (!password) {
-      return res.status(401).json({ message: '비밀번호 필수 입력' });
-    }
 
     const user = await User.findOne({
       where: {
@@ -79,9 +54,7 @@ module.exports = {
       .catch(err => console.log(err));
 
     if (!isValidPassword) {
-      return res
-        .status(401)
-        .json({ message: '비밀번호가 일치하지 않습니다.' });
+      return res.status(401).json({ message: '비밀번호가 일치하지 않습니다.' });
     }
 
     const accToken = await createJwtToken(user.id);
@@ -97,7 +70,7 @@ module.exports = {
       .json({ accToken, message: '로그인 성공했습니다.' });
   },
   // 로그아웃
-  logout: async (req, res) =>{
+  logout: async (req, res) => {
     res.cookie('token', '');
     res.status(200).json({ message: 'User has been logged out' });
   },
@@ -108,7 +81,7 @@ module.exports = {
       return res.status(404).json({ message: 'User not found' });
     }
     res.status(200).json({ token: req.token, username: user.username });
-  }
+  },
 };
 
 async function createJwtToken(id) {
