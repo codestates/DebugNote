6ead06cd828c2a3eba926b7fadc82ffef0b27eff
JeongTaeYeong <bellamy828@gmail.com>
2022-04-07 13:32:48 +0900

    Client [ADD] Main page, css

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 
-import Main from './Pages/';
+import Main from './Pages/Main';
 
 function App() {
+  const [isLogin, setIsLogin] = useState(false);
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<Main isLogin={isLogin} />} />
         <Route path="mypage" element>
           <Route index element />
           <Route path="info" element />
