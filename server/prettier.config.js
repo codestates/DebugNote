3fb4d820d240f5961083a0cfc6fbf1ebdcb1224e
su <abcc0785@gmail.com>
2022-04-14 14:40:17 +0900

    Client [FIX] client auth

--- server/prettier.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  tabWidth: 2,
-  endOfLine: 'lf',
-  arrowParens: 'avoid',
-  singleQuote: true,
-  semi: true,
-  trailingComma: 'all',
-  printWidth: 80,
-};
--- server/routes/search.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const searchController = require('../controllers/search');
-// const isAuth = require('../middlewares/auth');
-
-router.get('/', searchController.get);
-
-module.exports = router;
